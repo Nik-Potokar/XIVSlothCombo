faf97e4c68bcfb838a98ebdea6c61f970b6bc033
Ele <61023752+ele-starshade@users.noreply.github.com>
2022-02-06 19:56:10 +0000

    fix(BRD): [PUSH] simple bard opener stuckness

--- XIVSlothCombo/Combos/BRD.cs
+++ XIVSlothCombo/Combos/BRD.cs
@@ -401,10 +401,15 @@ namespace XIVSlothComboPlugin.Combos
                 if (inCombat && (lastComboMove == BRD.Ladonsbite || lastComboMove == BRD.QuickNock))
                 {
                     SimpleBardFeature.openerFinished = true;
+                    SimpleBardFeature.inOpener = true;
                 }
 
                 if (!inCombat)
                 {
+                    SimpleBardFeature.inOpener = false;
+                    SimpleBardFeature.step = 0;
+                    SimpleBardFeature.subStep = 0;
+                    SimpleBardFeature.usedStraightShotReady = false;
                     SimpleBardFeature.openerFinished = false;
                 }
 
@@ -511,11 +516,11 @@ namespace XIVSlothComboPlugin.Combos
     internal class SimpleBardFeature : CustomCombo
     {
         protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.SimpleBardFeature;
-        internal bool inOpener = false;
+        internal static bool inOpener = false;
         internal static bool openerFinished = false;
-        internal byte step = 0;
-        internal byte subStep = 0;
-        internal bool usedStraightShotReady = false;
+        internal static byte step = 0;
+        internal static byte subStep = 0;
+        internal static bool usedStraightShotReady = false;
 
         protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
         {
@@ -579,7 +584,7 @@ namespace XIVSlothComboPlugin.Combos
                             }
                             if (subStep == 3)
                             {
-                                if (lastComboMove == BRD.EmpyrealArrow) subStep++;
+                                if (IsOnCooldown(BRD.EmpyrealArrow)) subStep++;
                                 else return BRD.EmpyrealArrow;
                             }
                             if (subStep == 4)
@@ -734,7 +739,7 @@ namespace XIVSlothComboPlugin.Combos
                             }
                             if (subStep == 9)
                             {
-                                if (lastComboMove == BRD.EmpyrealArrow) subStep++;
+                                if (IsOnCooldown(BRD.EmpyrealArrow)) subStep++;
                                 else return BRD.EmpyrealArrow;
                             }
                             if (subStep == 10)
@@ -835,7 +840,7 @@ namespace XIVSlothComboPlugin.Combos
                             }
                             if (subStep == 8)
                             {
-                                if (lastComboMove == BRD.EmpyrealArrow) subStep++;
+                                if (IsOnCooldown(BRD.EmpyrealArrow)) subStep++;
                                 else return BRD.EmpyrealArrow;
                             }
                             if (subStep == 9)
