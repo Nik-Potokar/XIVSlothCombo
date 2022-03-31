bc2b9bb5b6978d472edae2d152a485dec21bb5ab
Taurenkey <github@taurenkey.co.uk>
2022-03-31 21:55:45 +0100

    Fix 2: Electric Boogaloo

--- XIVSlothCombo/ConfigWindow.cs
+++ XIVSlothCombo/ConfigWindow.cs
@@ -269,6 +269,7 @@ namespace XIVSlothComboPlugin
                     var conflictInfo = conflict.GetAttribute<CustomComboInfoAttribute>();
                     if (irlsloth) 
                     {
+                        //kek
                         return $"\n - {conflictInfo.MemeName}";
                     }
                     else
