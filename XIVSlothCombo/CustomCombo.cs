dfbe6151d1b48dc06b5ee3f3e29b978fd8e189e7
damolitionn <d.yap1993@gmail.com>
2022-04-06 07:16:51 -0500

    DRK: Opener

--- XIVSlothCombo/CustomCombo.cs
+++ XIVSlothCombo/CustomCombo.cs
@@ -302,6 +302,11 @@ namespace XIVSlothComboPlugin.Combos
         /// <returns>A value indicating if the effect exists.</returns>
         protected static bool HasEffect(ushort effectID)
             => FindEffect(effectID) is not null;
+        protected static float GetBuffStacks(ushort effectId)
+        {
+            Status? eff = FindEffect(effectId);
+            return eff?.StackCount ?? 0;
+        }
 
         /// <summary>
         /// Finds an effect on the player.
