a9aca5063b9969df8efce25519eb551a048cac1d
StevieCoops <Stevie-Coops@hotmail.com>
2022-04-18 00:27:53 -0700

    MCH Purify in PVP
    Added Purify and removed outdated combos

--- XIVSlothCombo/CombosPVP/MCHPVP.cs
+++ XIVSlothCombo/CombosPVP/MCHPVP.cs
@@ -7,69 +7,58 @@ namespace XIVSlothComboPlugin.Combos
         public const byte JobID = 31;
 
         public const uint
-            SplitShot = 8848,
-            SlugShot = 8849,
-            CleanShot = 8850,
-            Drill = 17749,
-            Airanchor = 17750,
-            SpreadShot = 18932,
-            Bioblaster = 17752,
-            GaussRound = 18933,
-            Ricochet = 17753,
-            Wildfire = 8855,
-            Blank = 8853;
-
-
+            
+            Purify = 29056,
+            BlastCharge = 29402,
+            HeatBlast = 29403,
+            Scattergun = 29404,
+            Drill = 29405,
+            Bioblaster = 29406,
+            AirAnchor = 29407,
+            ChainSaw = 29408,
+            Wildfire = 29409,
+            Recuperate = 29711,
+            BishopAutoturrent = 29412,
+            Analysis = 29414,
+            MarkmansSpite = 29415;
+        
         public static class Buffs
         {
             public const ushort
-                Concentrate = 2186;
+                DrillPrimed = 3150,
+                BioblasterPrimed = 3151,
+                AirAnchorPrimed = 3152,
+                ChainSawPrimed = 3153;
         }
 
         public static class Debuffs
         {
             public const ushort
-                Wildfire = 1323;
+                Wildfire = 1323,
+                Stun = 1343,
+                Heavy = 1344,
+                Bind = 1345,
+                Silence = 1347,
+                Sleep = 1348,
+                HalfAsleep = 3022,
+                DeepFreeze = 3219;
         }
     }
-    internal class HeatedCleanShotFeature : CustomCombo
+    
+    internal class PurifyFeature : CustomCombo
     {
-        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.HeatedCleanShotFeature;
+        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.PurifyFeature;
 
         protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
         {
-            if (actionID == MCHPVP.SplitShot || actionID == MCHPVP.SlugShot || actionID == MCHPVP.CleanShot)
+            if (actionID == MCHPVP.BlastCharge)
             {
                 var actionIDCD = GetCooldown(actionID);
-                var gaussCD = GetCooldown(MCHPVP.GaussRound);
-                var ricoCD = GetCooldown(MCHPVP.Ricochet);
-                var drillCD = GetCooldown(MCHPVP.Drill);
-                var airCD = GetCooldown(MCHPVP.Airanchor);
                 var incombat = HasCondition(Dalamud.Game.ClientState.Conditions.ConditionFlag.InCombat);
-                if (actionIDCD.IsCooldown && gaussCD.CooldownRemaining < ricoCD.CooldownRemaining)
-                    return MCHPVP.GaussRound;
-                else
-                    if(actionIDCD.IsCooldown)
-                    return MCHPVP.Ricochet;
-                if (HasEffect(MCHPVP.Buffs.Concentrate) && !drillCD.IsCooldown)
-                    return MCHPVP.Drill;
-                if (!airCD.IsCooldown && incombat || HasEffect(MCHPVP.Buffs.Concentrate) && !airCD.IsCooldown)
-                    return MCHPVP.Airanchor;
-            }
-
-            return actionID;
-        }
-    }
-    internal class WildfireBlankFeature : CustomCombo
-    {
-        protected internal override CustomComboPreset Preset { get; } = CustomComboPreset.WildfireBlankFeature;
-
-        protected override uint Invoke(uint actionID, uint lastComboMove, float comboTime, byte level)
-        {
-            if (actionID == MCHPVP.Wildfire)
-            {
-                if (TargetHasEffect(MCHPVP.Debuffs.Wildfire) && InMeleeRange(true))
-                    return MCHPVP.Blank;
+                if ((HasEffectAny(MCHPVP.Debuffs.Stun) || HasEffectAny(MCHPVP.Debuffs.Heavy) || HasEffectAny(MCHPVP.Debuffs.Bind) || HasEffectAny(MCHPVP.Debuffs.Silence) || HasEffectAny(MCHPVP.Debuffs.Sleep) || HasEffectAny(MCHPVP.Debuffs.HalfAsleep) || HasEffectAny(MCHPVP.Debuffs.DeepFreeze)) && IsOffCooldown(MCHPVP.Purify))
+                {
+                    return MCHPVP.Purify;
+                }
             }
 
             return actionID;
