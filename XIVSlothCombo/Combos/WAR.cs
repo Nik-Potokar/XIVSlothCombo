036863dc99dbeeccedb6dea8a58af72d8a6b44f8
damolitionn <d.yap1993@gmail.com>
2022-04-20 19:51:12 -0500

    WAR: Overcap Fix for below 50

--- XIVSlothCombo/Combos/WAR.cs
+++ XIVSlothCombo/Combos/WAR.cs
@@ -139,7 +139,7 @@ namespace XIVSlothComboPlugin.Combos
 
                     if (lastComboMove == WAR.Maim && level >= WAR.Levels.StormsPath)
                     {
-                        if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapFeature) && level >= WAR.Levels.InnerBeast && HasEffectAny(WAR.Buffs.SurgingTempest) && gauge >= 90)
+                        if (IsEnabled(CustomComboPreset.WarriorGaugeOvercapFeature) && level >= WAR.Levels.InnerBeast && (level < WAR.Levels.StormsEye || HasEffectAny(WAR.Buffs.SurgingTempest)) && gauge >= 90)
                             return OriginalHook(WAR.InnerBeast);
                         if ((GetBuffRemainingTime(WAR.Buffs.SurgingTempest) <= surgingThreshold) && level >= WAR.Levels.StormsEye)
                             return WAR.StormsEye;
