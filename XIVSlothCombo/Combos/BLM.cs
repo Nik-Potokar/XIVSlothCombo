fea834b2ef4f069e9030774e478f1c84f81f690a
grimgal <dottedecho@gmail.com>
2022-04-29 11:35:07 -0700

    MP check for Flare

--- XIVSlothCombo/Combos/BLM.cs
+++ XIVSlothCombo/Combos/BLM.cs
@@ -375,7 +375,7 @@ namespace XIVSlothComboPlugin.Combos
                     {
                         return level >= BLM.Levels.HighFire2 ? BLM.HighFire2 : BLM.Fire2;
                     }
-                    else
+                    else if (currentMP >= BLM.MP.AspectFire)
                     {
                         return BLM.Flare;
                     }
