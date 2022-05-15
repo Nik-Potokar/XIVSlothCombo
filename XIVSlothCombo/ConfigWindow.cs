668503120defb8bd80e6ccdf1a5524a742590299
k-kz <68086259+k-kz@users.noreply.github.com>
2022-05-15 08:37:40 +0100

    v3.0.15.0 Pre-Update
    Version number in `About`
    Welcome message
    RPR & BLU rework notes
    
    Changelog:
    Features, Fixes & Enhancements for BLM, BLU, BRD, MCH, MNK, PLD, RDM, RPR, SAM, SCH, SGE, and SMN.

--- XIVSlothCombo/ConfigWindow.cs
+++ XIVSlothCombo/ConfigWindow.cs
@@ -261,7 +261,7 @@ namespace XIVSlothComboPlugin
         {
             ImGui.BeginChild("about", new Vector2(0, 0), true);
 
-            ImGui.TextColored(ImGuiColors.ParsedGreen, $"v3.0.14.1\n- with love from Team Sloth.");
+            ImGui.TextColored(ImGuiColors.ParsedGreen, $"v3.0.15.0\n- with love from Team Sloth.");
             ImGui.Spacing();
             ImGui.Spacing();
             ImGui.Spacing();
