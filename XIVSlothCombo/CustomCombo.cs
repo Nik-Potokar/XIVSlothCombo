10f28ad4ca90987b067b1640a079bddc5aae77aa
Tsusai <tsusai@gmail.com>
2022-05-22 16:00:41 -0500

    Card Draw Targetting speedup AST: Moved Job names into CustomCombo as List groups (JobNames.Melee for example). In case it's needed in the future, any other feature can check the names.
    AstrologianCardsOnDrawFeaturelikewhat SetTarget: Checking job names via new lists
    Before info:
    -EN: 22 Ticks Average to find melee job
    -EN melee being checked after CN and JP: 50 ticks average
    After: EN melee (after CN & JP): 2 ticks average (vs 50)

--- XIVSlothCombo/CustomCombo.cs
+++ XIVSlothCombo/CustomCombo.cs
@@ -880,6 +880,56 @@ namespace XIVSlothComboPlugin.Combos
         public bool IsSpellActive(uint id)
             => Service.Configuration.ActiveBLUSpells.Contains(id);
 
+        //Class Names
+        public class JobNames
+        {
+            public static readonly List<string> Melee = new() {
+                //English
+                "monk", "dragoon", "ninja", "reaper", "samurai", "pugilist", "lancer", "rogue",
+                //Chinese
+                "武僧", "龙骑士", "忍者", "钐镰客", "武士", "格斗家", "枪术师", "双剑师",
+                //Japanese
+                "モンク", "竜騎士", "忍者", "リーパー", "侍", "格闘士", "槍術士", "双剣士",
+                //French
+                //German
+            };
+
+            public static readonly List<string> Ranged = new() {
+                //English
+                "bard", "machinist", "dancer", "red mage", "black mage", "summoner", "blue mage", "archer", "thaumaturge", "arcanist",
+                //Chinese
+                "吟游诗人", "机工士", "舞者", "赤魔法师", "黑魔法师", "召唤师", "青魔法师", "弓箭手", "咒术师", "秘术师",
+                //Japanese
+                "吟遊詩人", "機工士", "踊り子", "赤魔道士", "黒魔道士", "召喚士", "青魔道士", "弓術士", "呪術士", "巴術士"
+                //French
+                //German
+            };
+
+            public static readonly List<string> Tank = new()
+            {
+                //English
+                "paladin", "warrior", "dark knight", "gunbreaker", "gladiator", "marauder",
+                //Chinese
+                "骑士", "战士", "暗黑骑士", "绝枪战士", "剑术师", "斧术师",
+                //Japanese
+                "ナイト", "戦士", "暗黒騎士", "ガンブレイカー", "剣術士", "斧術士"
+                //French
+                //German
+            };
+            
+            public static readonly List<string> Healer = new()
+            {
+                //English
+                "white mage","astrologian","scholar","sage","conjurer",
+                //Chinese
+                "白魔法师","占星术士","学者","贤者","幻术师",
+                //Japanese
+                "白魔道士","占星術師","学者","賢者","幻術士"
+                //French
+                //German
+            };
+        }
+
         //public bool CanUseAction(uint id)
         //{
         //    if (!ActionWatching.ActionSheet.TryGetValue(id, out var actionFromSheet)) return false;
