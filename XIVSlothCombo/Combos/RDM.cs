81f901f33c4d89c9192ce2f53b9b4f3310cc8cfb
NikkoToska <61327453+NikkoXIV@users.noreply.github.com>
2022-06-08 23:43:00 +0900

    No more extra Vs, Cupcake ruined the Vs for everyone.

--- XIVSlothCombo/Combos/RDM.cs
+++ XIVSlothCombo/Combos/RDM.cs
@@ -781,14 +781,14 @@ namespace XIVSlothComboPlugin.Combos
                 }
                 //END_RDM_VERTHUNDERVERAERO
 
-                //RDM_VERTHUNDERIIVVERAEROII
+                //RDM_VERTHUNDERIIVERAEROII
                 if (IsEnabled(CustomComboPreset.RDM_VerthunderIIVeraeroII) 
                     && actionID is Scatter or Impact)
                 {
                     if (useThunder2) return Verthunder2;
                     if (useAero2) return Veraero2;
                 }
-                //END_RDM_VERTHUNDERIIVVERAEROII
+                //END_RDM_VERTHUNDERIIVERAEROII
 
 
                 //NO_CONDITIONS_MET
