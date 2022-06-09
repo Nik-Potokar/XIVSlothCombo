c39807924e59496304c58606baeffe9594c935e1
k-kz <68086259+k-kz@users.noreply.github.com>
2022-06-09 18:44:16 +0100

    [Framework] Update summary tags (Service.cs)
    Turned all into one-line

--- XIVSlothCombo/Service.cs
+++ XIVSlothCombo/Service.cs
@@ -16,105 +16,69 @@ using System.Reflection;
 
 namespace XIVSlothComboPlugin
 {
-    /// <summary>
-    /// Dalamud and plugin services.
-    /// </summary>
+    /// <summary> Dalamud and plugin services. </summary>
     internal class Service
     {
-        /// <summary>
-        /// Gets or sets the plugin address resolver.
-        /// </summary>
+        /// <summary> Gets or sets the plugin address resolver. </summary>
         internal static PluginAddressResolver Address { get; set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud buddy list.
-        /// </summary>
+        /// <summary> Gets the Dalamud buddy list. </summary>
         [PluginService]
         internal static BuddyList BuddyList { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud chat gui.
-        /// </summary>
+        /// <summary> Gets the Dalamud chat gui. </summary>
         [PluginService]
         internal static ChatGui ChatGui { get; private set; } = null!;
 
-        /// <summary>
-        /// Facilitates class-based locking.
-        /// </summary>
+        /// <summary> Facilitates class-based locking. </summary>
         internal static bool ClassLocked { get; set; } = true;
 
-        /// <summary>
-        /// Gets the Dalamud client state.
-        /// </summary>
+        /// <summary> Gets the Dalamud client state. </summary>
         [PluginService]
         internal static ClientState ClientState { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets or sets the plugin caching mechanism.
-        /// </summary>
+        /// <summary> Gets or sets the plugin caching mechanism. </summary>
         internal static CustomComboCache ComboCache { get; set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud command manager.
-        /// </summary>
+        /// <summary> Gets the Dalamud command manager. </summary>
         [PluginService]
         internal static CommandManager CommandManager { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud condition.
-        /// </summary>
+        /// <summary> Gets the Dalamud condition. </summary>
         [PluginService]
         internal static Condition Condition { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets or sets the plugin configuration.
-        /// </summary>
+        /// <summary> Gets or sets the plugin configuration. </summary>
         internal static PluginConfiguration Configuration { get; set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud data manager.
-        /// </summary>
+        /// <summary> Gets the Dalamud data manager. </summary>
         [PluginService]
         internal static DataManager DataManager { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud framework manager.
-        /// </summary>
+        /// <summary> Gets the Dalamud framework manager. </summary>
         [PluginService]
         internal static Framework Framework { get; private set; } = null!;
 
-        /// <summary>
-        /// Handles the in-game UI.
-        /// </summary>
+        /// <summary> Handles the in-game UI. </summary>
         [PluginService]
         internal static GameGui GameGui { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets or sets the plugin icon replacer.
-        /// </summary>
+        /// <summary> Gets or sets the plugin icon replacer. </summary>
         internal static IconReplacer IconReplacer { get; set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud plugin interface.
-        /// </summary>
+        /// <summary> Gets the Dalamud plugin interface. </summary>
         [PluginService]
         internal static DalamudPluginInterface Interface { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud job gauges.
-        /// </summary>
+        /// <summary> Gets the Dalamud job gauges. </summary>
         [PluginService]
         internal static JobGauges JobGauges { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud object table.
-        /// </summary>
+        /// <summary> Gets the Dalamud object table. </summary>
         [PluginService]
         internal static ObjectTable ObjectTable { get; private set; } = null!;
 
-        /// <summary>
-        /// Returns the Plugin Folder location
-        /// </summary>
+        /// <summary> Returns the Plugin Folder location </summary>
         public static string PluginFolder
         {
             get
@@ -126,21 +90,15 @@ namespace XIVSlothComboPlugin
             }
         }
 
-        /// <summary>
-        /// Gets the Dalamud party list.
-        /// </summary>
+        /// <summary> Gets the Dalamud party list. </summary>
         [PluginService]
         internal static PartyList PartyList { get; private set; } = null!;
 
-        /// <summary>
-        /// Facilitates searching for memory signatures.
-        /// </summary>
+        /// <summary> Facilitates searching for memory signatures. </summary>
         [PluginService]
         internal static SigScanner SigScanner { get; private set; } = null!;
 
-        /// <summary>
-        /// Gets the Dalamud target manager.
-        /// </summary>
+        /// <summary> Gets the Dalamud target manager. </summary>
         [PluginService]
         internal static TargetManager TargetManager { get; private set; } = null!;
     }
