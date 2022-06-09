3b76c200d0c67c7e9924a9ea0bb8166581313ae2
k-kz <68086259+k-kz@users.noreply.github.com>
2022-06-09 19:42:39 +0100

    Update PluginAddressResolver.cs
    More cleanup

--- XIVSlothCombo/PluginAddressResolver.cs
+++ XIVSlothCombo/PluginAddressResolver.cs
@@ -1,33 +1,22 @@
 using System;
-
 using Dalamud.Game;
 using Dalamud.Logging;
 
 namespace XIVSlothComboPlugin
 {
-    /// <summary>
-    /// Plugin address resolver.
-    /// </summary>
+    /// <summary> Plugin address resolver. </summary>
     internal class PluginAddressResolver : BaseAddressResolver
     {
-        /// <summary>
-        /// Gets the address of the member ComboTimer.
-        /// </summary>
+        /// <summary> Gets the address of the member ComboTimer. </summary>
         public IntPtr ComboTimer { get; private set; }
 
-        /// <summary>
-        /// Gets the address of the member LastComboMove.
-        /// </summary>
+        /// <summary> Gets the address of the member LastComboMove. </summary>
         public IntPtr LastComboMove => this.ComboTimer + 0x4;
 
-        /// <summary>
-        /// Gets the address of fpGetAdjustedActionId.
-        /// </summary>
+        /// <summary> Gets the address of fpGetAdjustedActionId. </summary>
         public IntPtr GetAdjustedActionId { get; private set; }
 
-        /// <summary>
-        /// Gets the address of fpIsIconReplacable.
-        /// </summary>
+        /// <summary> Gets the address of fpIsIconReplacable. </summary>
         public IntPtr IsActionIdReplaceable { get; private set; }
 
         /// <inheritdoc/>
@@ -39,7 +28,7 @@ namespace XIVSlothComboPlugin
 
             this.IsActionIdReplaceable = scanner.ScanText("81 F9 ?? ?? ?? ?? 7F 35");
 
-            PluginLog.Verbose("===== X I V C O M B O =====");
+            PluginLog.Verbose("===== X I V S L O T H C O M B O =====");
             PluginLog.Verbose($"{nameof(this.GetAdjustedActionId)}   0x{this.GetAdjustedActionId:X}");
             PluginLog.Verbose($"{nameof(this.IsActionIdReplaceable)} 0x{this.IsActionIdReplaceable:X}");
             PluginLog.Verbose($"{nameof(this.ComboTimer)}            0x{this.ComboTimer:X}");
