333c9ffc858c3535e67ed2c74ffeab44be01616a
k-kz <68086259+k-kz@users.noreply.github.com>
2022-06-09 18:12:10 +0100

    [Framework] More tidying (Service.cs)
    Ordered class contents alphabetically
    Added missing <summary> descriptions
    Removed redundant namespace ref

--- XIVSlothCombo/Service.cs
+++ XIVSlothCombo/Service.cs
@@ -8,7 +8,6 @@ using Dalamud.Game.ClientState.Objects;
 using Dalamud.Game.ClientState.Party;
 using Dalamud.Game.Command;
 using Dalamud.Game.Gui;
-using Dalamud.Interface;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System;
@@ -23,30 +22,9 @@ namespace XIVSlothComboPlugin
     internal class Service
     {
         /// <summary>
-        /// Gets or sets the plugin configuration.
-        /// </summary>
-        internal static PluginConfiguration Configuration { get; set; } = null!;
-
-        /// <summary>
-        /// Gets or sets the plugin caching mechanism.
-        /// </summary>
-        internal static CustomComboCache ComboCache { get; set; } = null!;
-
-        /// <summary>
-        /// Gets or sets the plugin icon replacer.
-        /// </summary>
-        internal static IconReplacer IconReplacer { get; set; } = null!;
-
-        /// <summary>
         /// Gets or sets the plugin address resolver.
         /// </summary>
         internal static PluginAddressResolver Address { get; set; } = null!;
-
-        /// <summary>
-        /// Gets the Dalamud plugin interface.
-        /// </summary>
-        [PluginService]
-        internal static DalamudPluginInterface Interface { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud buddy list.
@@ -61,10 +39,20 @@ namespace XIVSlothComboPlugin
         internal static ChatGui ChatGui { get; private set; } = null!;
 
         /// <summary>
+        /// Facilitates class-based locking.
+        /// </summary>
+        internal static bool ClassLocked { get; set; } = true;
+
+        /// <summary>
         /// Gets the Dalamud client state.
         /// </summary>
         [PluginService]
         internal static ClientState ClientState { get; private set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the plugin caching mechanism.
+        /// </summary>
+        internal static CustomComboCache ComboCache { get; set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud command manager.
@@ -79,6 +67,11 @@ namespace XIVSlothComboPlugin
         internal static Condition Condition { get; private set; } = null!;
 
         /// <summary>
+        /// Gets or sets the plugin configuration.
+        /// </summary>
+        internal static PluginConfiguration Configuration { get; set; } = null!;
+
+        /// <summary>
         /// Gets the Dalamud data manager.
         /// </summary>
         [PluginService]
@@ -91,6 +84,23 @@ namespace XIVSlothComboPlugin
         internal static Framework Framework { get; private set; } = null!;
 
         /// <summary>
+        /// Handles the in-game UI.
+        /// </summary>
+        [PluginService]
+        internal static GameGui GameGui { get; private set; } = null!;
+
+        /// <summary>
+        /// Gets or sets the plugin icon replacer.
+        /// </summary>
+        internal static IconReplacer IconReplacer { get; set; } = null!;
+
+        /// <summary>
+        /// Gets the Dalamud plugin interface.
+        /// </summary>
+        [PluginService]
+        internal static DalamudPluginInterface Interface { get; private set; } = null!;
+
+        /// <summary>
         /// Gets the Dalamud job gauges.
         /// </summary>
         [PluginService]
@@ -101,15 +111,6 @@ namespace XIVSlothComboPlugin
         /// </summary>
         [PluginService]
         internal static ObjectTable ObjectTable { get; private set; } = null!;
-
-        /// <summary>
-        /// Gets the Dalamud target manager.
-        /// </summary>
-        [PluginService]
-        internal static TargetManager TargetManager { get; private set; } = null!;
-
-        [PluginService]
-        internal static SigScanner SigScanner { get; private set; } = null!;
 
         /// <summary>
         /// Returns the Plugin Folder location
@@ -131,10 +132,16 @@ namespace XIVSlothComboPlugin
         [PluginService]
         internal static PartyList PartyList { get; private set; } = null!;
 
+        /// <summary>
+        /// Facilitates searching for memory signatures.
+        /// </summary>
         [PluginService]
-        internal static GameGui GameGui { get; private set; } = null!;
+        internal static SigScanner SigScanner { get; private set; } = null!;
 
-        internal static bool ClassLocked { get; set; } = true;
-
+        /// <summary>
+        /// Gets the Dalamud target manager.
+        /// </summary>
+        [PluginService]
+        internal static TargetManager TargetManager { get; private set; } = null!;
     }
 }
