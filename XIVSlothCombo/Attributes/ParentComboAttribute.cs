e670865d6ae34ee856e8d3c62a2c0803f1a4766a
k-kz <68086259+k-kz@users.noreply.github.com>
2022-06-10 02:16:07 +0100

    Attributes folder cleanup
    Removed `TrustIncompatibleAttribute.cs`
    Summary tag cleanup
    Other code cleaned
    Removed redundant namespace refs
    Converted single expressions to `=>`

--- XIVSlothCombo/Attributes/ParentComboAttribute.cs
+++ XIVSlothCombo/Attributes/ParentComboAttribute.cs
@@ -2,24 +2,15 @@ using System;
 
 namespace XIVSlothComboPlugin.Attributes
 {
-    /// <summary>
-    /// Attribute documenting required combo relationships.
-    /// </summary>
+    /// <summary> Attribute documenting required combo relationships. </summary>
     [AttributeUsage(AttributeTargets.Field)]
     internal class ParentComboAttribute : Attribute
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ParentComboAttribute"/> class.
-        /// </summary>
+        /// <summary> Initializes a new instance of the <see cref="ParentComboAttribute"/> class. </summary>
         /// <param name="parentPreset">Presets that conflict with the given combo.</param>
-        internal ParentComboAttribute(CustomComboPreset parentPreset)
-        {
-            this.ParentPreset = parentPreset;
-        }
+        internal ParentComboAttribute(CustomComboPreset parentPreset) => ParentPreset = parentPreset;
 
-        /// <summary>
-        /// Gets the display name.
-        /// </summary>
+        /// <summary> Gets the display name. </summary>
         public CustomComboPreset ParentPreset { get; }
     }
 }
--- XIVSlothCombo/Attributes/ReplaceSkillAttribute.cs
+++ XIVSlothCombo/Attributes/ReplaceSkillAttribute.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using XIVSlothComboPlugin;
 
 namespace XIVSlothComboPlugin
 {
-    /// <summary>
-    /// Attribute documenting which skill each preset replace.
-    /// </summary>
+    /// <summary> Attribute documenting which skill each preset replace. </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class ReplaceSkillAttribute : Attribute
     {
@@ -17,26 +12,19 @@ namespace XIVSlothComboPlugin
                     .Where(i => i.ClassJobCategory.Row > 0 && i.ActionCategory.Row <= 4 && i.RowId is not 7)
                     .ToDictionary(i => i.RowId, i => i);
 
-        /// <summary>
-        /// List of each action the feature replaces. Initializes a new instance of the <see cref="ReplaceSkillAttribute"/> class.
-        /// </summary>
+        /// <summary> List of each action the feature replaces. Initializes a new instance of the <see cref="ReplaceSkillAttribute"/> class. </summary>
         /// <param name="actionIDs">List of actions the preset replaces</param>
         internal ReplaceSkillAttribute(params uint[] actionIDs)
         {
             foreach(uint id in actionIDs)
             {
-                if (ActionSheet.TryGetValue(id, out var action))
+                if (ActionSheet.TryGetValue(id, out var action) && action != null)
                 {
-                    if (action != null)
-                    {
-                        ActionNames.Add($"{action.Name}");
-                    }
+                    ActionNames.Add($"{action.Name}");
                 }
             }
-
         }
 
         internal List<string> ActionNames { get; set; } = new();
-
     }
 }
