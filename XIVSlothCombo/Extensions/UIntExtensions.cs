ed25880fffd874f682ff3c825a541ee3e39b0d5a
Taurenkey <github@taurenkey.co.uk>
2022-06-11 23:46:12 +0100

    Start of extension methods

---
+++ XIVSlothCombo/Extensions/UIntExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XIVSlothCombo.CustomComboNS.Functions;
+
+namespace XIVSlothCombo.Extensions
+{
+    internal static class UIntExtensions
+    {
+        internal static bool LevelChecked(this uint value) => CustomComboFunctions.LevelChecked(value);
+    }
+}
