4d02ade11d074a1604916b7bc6a36b940e1e97eb
Taurenkey <github@taurenkey.co.uk>
2022-08-27 15:01:44 +0100

    Movement check fixed

--- XIVSlothCombo/CustomCombo/Functions/Movement.cs
+++ XIVSlothCombo/CustomCombo/Functions/Movement.cs
@@ -10,19 +10,9 @@ namespace XIVSlothCombo.CustomComboNS.Functions
         public static bool IsMoving { get; set; }
 
         /// <summary> Checks player movement </summary>
-        public void CheckMovement()
+        public unsafe void CheckMovement()
         {
-            if (MovingCounter == 0)
-            {
-                Vector2 newPosition = LocalPlayer is null ? Vector2.Zero : new Vector2(LocalPlayer.Position.X, LocalPlayer.Position.Z);
-                PlayerSpeed = Vector2.Distance(newPosition, Position);
-                IsMoving = PlayerSpeed > 0;
-                Position = LocalPlayer is null ? Vector2.Zero : newPosition;
-                MovingCounter = 50; // Refreshes every 50 Dalamud ticks for a more accurate representation of speed, otherwise it'll report 0.
-            }
-
-            if (MovingCounter > 0)
-                MovingCounter--;
+            IsMoving = FFXIVClientStructs.FFXIV.Client.UI.Agent.AgentMap.Instance()->IsPlayerMoving > 0;
         }
     }
 }
