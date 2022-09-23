858217a7de524cea83865932112610f876c8ddb6
Taurenkey <github@taurenkey.co.uk>
2022-09-23 06:48:38 +0100

    Fix phantom kamaitachi bug

--- XIVSlothCombo/Combos/PvE/NIN.cs
+++ XIVSlothCombo/Combos/PvE/NIN.cs
@@ -315,10 +315,9 @@ namespace XIVSlothCombo.Combos.PvE
 
                     if (IsEnabled(CustomComboPreset.NIN_ST_AdvancedMode_Bunshing_Phantom) &&
                         HasEffect(Buffs.PhantomReady) &&
-                        ((GetCooldownRemainingTime(TrickAttack) > GetBuffRemainingTime(Buffs.PhantomReady)) || TargetHasEffect(Debuffs.TrickAttack)) &&
+                        ((GetCooldownRemainingTime(TrickAttack) > GetBuffRemainingTime(Buffs.PhantomReady)) || GetBuffRemainingTime(Buffs.PhantomReady) < 5 || TargetHasEffect(Debuffs.TrickAttack) || !InMeleeRange() || (HasEffect(Buffs.Bunshin) && TargetHasEffect(Debuffs.Mug))) &&
                         PhantomKamaitachi.LevelChecked())
                         return OriginalHook(PhantomKamaitachi);
-
 
                     if (!inTNWithDoubleArmorCrush)
                     {
