e94b199bd7c26a9c597778c922b5a98627ad7cf8
Daemeous <daemeous@gmail.com>
2022-10-07 14:57:17 +0100

    Changed priority to Assize first.

--- XIVSlothCombo/Combos/PvE/WHM.cs
+++ XIVSlothCombo/Combos/PvE/WHM.cs
@@ -315,10 +315,10 @@ namespace XIVSlothCombo.Combos.PvE
 
                     if (CanSpellWeave(actionID) || IsMoving)
                     {
-                        if (IsEnabled(CustomComboPreset.WHM_AoE_DPS_PresenceOfMind) && ActionReady(PresenceOfMind))
-                            return PresenceOfMind;
                         if (IsEnabled(CustomComboPreset.WHM_AoE_DPS_Assize) && ActionReady(Assize))
                             return Assize;
+                        if (IsEnabled(CustomComboPreset.WHM_AoE_DPS_PresenceOfMind) && ActionReady(PresenceOfMind))
+                            return PresenceOfMind;
                         if (IsEnabled(CustomComboPreset.WHM_AoE_DPS_Lucid) && ActionReady(All.LucidDreaming) && LocalPlayer.CurrentMp <= PluginConfiguration.GetCustomIntValue(Config.WHM_AoE_Lucid))
                             return All.LucidDreaming;
                     }
