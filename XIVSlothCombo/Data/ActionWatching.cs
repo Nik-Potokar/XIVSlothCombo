8de3ecc00ab5a6987691f4bb7ef151f59c2af77b
Tartarga <109563717+Tartarga@users.noreply.github.com>
2022-10-12 14:12:29 -0500

    Comment Cleanup

--- XIVSlothCombo/Data/ActionWatching.cs
+++ XIVSlothCombo/Data/ActionWatching.cs
@@ -171,7 +171,7 @@ namespace XIVSlothCombo.Data
 
         public static int GetLevel(uint id) => ActionSheet.TryGetValue(id, out var action) && action.ClassJobCategory is not null ? action.ClassJobLevel : 255;
         public static int GetActionRange(uint id) => ActionSheet.TryGetValue(id, out var action) ? action.Range : -2; // 0 & -1 are valid numbers. -2 is our failure code for InActionRange
-        public static int GetActionRadius(uint id) => ActionSheet.TryGetValue(id, out var action) ? action.EffectRange : -1; //-1 is our failure code for.
+        public static int GetActionEffectRange(uint id) => ActionSheet.TryGetValue(id, out var action) ? action.EffectRange : -1;
         public static int GetTraitLevel(uint id) => TraitSheet.TryGetValue(id, out var trait) ? trait.Level : 255;
         public static string GetActionName(uint id) => ActionSheet.TryGetValue(id, out var action) ? (string)action.Name : "UNKNOWN ABILITY";
         public static string GetStatusName(uint id) => StatusSheet.TryGetValue(id, out var status) ? (string)status.Name : "Unknown Status";
