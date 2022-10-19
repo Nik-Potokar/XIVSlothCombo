a2beadda0d79c04f736ace15df7c2c14f78fa27b
Kagekazu <wijnsmattia@gmail.com>
2022-10-19 16:00:47 +0200

    final changes - some wording and white spaces solved. everything is ready for testing! (added the feedback from testing + from thread in here)

--- XIVSlothCombo/Combos/CustomComboPreset.cs
+++ XIVSlothCombo/Combos/CustomComboPreset.cs
@@ -1610,7 +1610,7 @@ namespace XIVSlothCombo.Combos
         MNK_ST_Simple_Thunderclap = 9033,
 
         [ParentCombo(MNK_AoE_Meditation)]
-        [CustomComboInfo("Mediation Uptime Feature", "Replaces Main Combo with Mediation when you are out of range and out of opener/burst.", MNK.JobID, 0, "", "")]
+        [CustomComboInfo("Mediation Uptime Feature", "Replaces Main Combo with Mediation when you are out of range.", MNK.JobID, 0, "", "")]
         MNK_AoE_Meditation_Uptime = 9034,
 
         #endregion
