0340a1143f3d71fd823091633fe432246ac95af8
Tartarga <109563717+Tartarga@users.noreply.github.com>
2022-10-20 13:39:55 -0500

    Sage ST DPS Enhancements Sage: - ST and AoE internal naming updated to match other healers ("Dosis"/"Phlegma" -> "DPS") - Created "Advanced Action Options" UI Option - Previous Alt Mode renamed and placed under "Advanced Action Options" - Integrated "Instant Casts" concepts out of #1042 into another Advanced Action Option on Toxikon - Toxikon Option is now Movement Options. Includes Toxikon, Dyskrasia and Eukrasia.

--- XIVSlothCombo/CustomCombo/Functions/Config.cs
+++ XIVSlothCombo/CustomCombo/Functions/Config.cs
@@ -13,4 +13,38 @@ namespace XIVSlothCombo.CustomComboNS.Functions
 
         public static float GetOptionFloat(string SliderID) => PluginConfiguration.GetCustomFloatValue(SliderID);
     }
+
+    internal class UserData
+    {
+        protected string pName;
+        public UserData(string v) => pName = v;
+        public static implicit operator string(UserData o) => (o.pName);
+    }
+
+    internal class UserFloat : UserData
+    {
+        public UserFloat(string v) : base(v) { }
+        public static implicit operator float(UserFloat o) => PluginConfiguration.GetCustomFloatValue(o.pName);
+    }
+
+    internal class UserInt : UserData
+    {
+        public UserInt(string v) : base(v) { }
+        public static implicit operator int(UserInt o) => PluginConfiguration.GetCustomIntValue(o.pName);
+    }
+
+    internal class UserBool : UserData
+    {
+        public UserBool(string v) : base(v) { }
+        public static implicit operator bool(UserBool o) => PluginConfiguration.GetCustomBoolValue(o.pName);
+    }
+
+    internal class UserBoolArray : UserData
+    {
+        public UserBoolArray(string v) : base(v) { }
+        public int Length => PluginConfiguration.GetCustomBoolArrayValue(this.pName).Length;
+        public bool this[int index] => PluginConfiguration.GetCustomBoolArrayValue(this.pName)[index];
+        public static implicit operator bool[](UserBoolArray o) => PluginConfiguration.GetCustomBoolArrayValue(o.pName);
+    }
+
 }
