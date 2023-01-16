cd32bbbc72d4a4c0ca9fd30290a73c247c486fed
Riley Edwards <jaredteam@gmail.com>
2023-01-15 22:28:33 -0600

    Added Bozja IDs for testing

---
+++ XIVSlothCombo/Combos/PvE/Bozja.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIVSlothCombo.Combos.PvE
+{
+    internal static class Bozja
+    {
+        public const uint
+            fontofpower = 20717,
+            fontofmagic = 20715,
+            bannerofhonoredsacrifice = 20721,
+            bannerofnobleends = 20720,
+            lostexcellence = 23919,
+            lostassassination = 23914,
+            lostseraphstrike = 22354,
+            lostchainspell = 23913,
+            lostrendarmor = 22353,
+            lostbloodrage = 23921,
+            lostflarestar = 22352,
+            lostdervish = 22356,
+            lostcure2 = 20727,
+            lostcure4 = 20729,
+            Lostrampage = 23910,
+            Lostburst = 23909;
+
+        public static class Buffs
+        {
+            public const ushort
+                fontofpower = 2346,
+                fontofmagic = 2332,
+                lostexcellence = 2564,
+                lostchainspell = 2560,
+                clericstance = 2484,
+                lostbloodrage = 2566,
+                lostaethershield = 2443,
+                lostbravery2 = 2341,
+                beastessence = 2324,
+                bannerofhonoredsacrifice = 2327,
+                bannerofnobleends = 2326;
+
+        }
+
+        public static class Debuffs
+        {
+            public const ushort
+                lostflarestar = 2440,
+                lostrendarmor = 2441;
+        }
+
+
+    }
+}
