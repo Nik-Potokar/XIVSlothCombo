92e5ab6c1ead35e60fe07ceaf13247c460bb652f
Riley Edwards <jaredteam@gmail.com>
2023-01-23 15:53:22 -0600

    there seems to have been a error that added a file, removed the file

--- XIVSlothCombo/Combos/PvE/NIN.Bozja_stuffs.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace XIVSlothCombo.Combos.PvE
-{
-    internal partial class NIN
-    {
-        internal class Bozja_stuffs                    }
-
-                    if                     }
