945fe1f1053bc2f0bc0659a4dfdabc410d632ed8
Taurenkey <github@taurenkey.co.uk>
2023-06-11 19:51:48 +0100

    Fix GetAttackType for non-English clients

--- XIVSlothCombo/Data/ActionWatching.cs
+++ XIVSlothCombo/Data/ActionWatching.cs
@@ -244,11 +244,11 @@ namespace XIVSlothCombo.Data
         {
             if (!ActionSheet.TryGetValue(id, out var action)) return ActionAttackType.Unknown;
 
-            return action.ActionCategory.Value.Name.RawString switch
+            return action.ActionCategory.Row switch
             {
-                "Spell" => ActionAttackType.Spell,
-                "Weaponskill" => ActionAttackType.Weaponskill,
-                "Ability" => ActionAttackType.Ability,
+                2 => ActionAttackType.Spell,
+                3 => ActionAttackType.Weaponskill,
+                4 => ActionAttackType.Ability,
                 _ => ActionAttackType.Unknown
             };
         }
