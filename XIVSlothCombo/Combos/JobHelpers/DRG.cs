3edfe00d33e4904346da8b10491a3c2ecae7bcce
Taurenkey <github@taurenkey.co.uk>
2023-07-26 20:35:00 +0100

    Add DRGHelper

---
+++ XIVSlothCombo/Combos/JobHelpers/DRG.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using XIVSlothCombo.CustomComboNS.Functions;
+using XIVSlothCombo.Data;
+
+namespace XIVSlothCombo.Combos.JobHelpers
+{
+    internal class DRG
+    {
+        internal static readonly List<uint> FastLocks = new()
+        {
+            PvE.DRG.BattleLitany,
+            PvE.DRG.LanceCharge,
+            PvE.DRG.DragonSight,
+            PvE.DRG.LifeSurge,
+            PvE.DRG.Geirskogul,
+            PvE.DRG.Nastrond,
+            PvE.DRG.MirageDive,
+            PvE.DRG.WyrmwindThrust
+        };
+
+        internal static readonly List<uint> MidLocks = new()
+        {
+            PvE.DRG.Jump,
+            PvE.DRG.HighJump,
+            PvE.DRG.DragonfireDive,
+            PvE.DRG.SpineshatterDive
+        };
+
+        internal static uint SlowLock => PvE.DRG.SpineshatterDive;
+
+
+        internal static bool CanDRGWeave(uint oGCD)
+        {
+            //GCD Ready - No Weave
+            if (CustomComboFunctions.IsOffCooldown(PvE.DRG.TrueThrust))
+                return false;
+
+            var gcdTimer = CustomComboFunctions.GetCooldownRemainingTime(PvE.DRG.TrueThrust);
+
+            if (FastLocks.Any(x => x == oGCD) && gcdTimer >= 0.6f)
+                return true;
+
+            if (MidLocks.Any(x => x == oGCD) && gcdTimer >= 0.8f)
+                return true;
+
+            if (SlowLock == oGCD && gcdTimer >= 1.5f)
+                return true;
+
+            return false;
+        }
+    }
+}
