8dea31452bb2f761acb918aadb75bea97996cb95
Kage <wijnsmattia@gmail.com>
2023-09-01 19:49:11 +0200

    fix standard and 123 opener

--- XIVSlothCombo/Combos/PvE/MCH.cs
+++ XIVSlothCombo/Combos/PvE/MCH.cs
@@ -445,7 +445,8 @@ namespace XIVSlothCombo.Combos.PvE
                 if (actionID is SplitShot)
                 {
                     if (IsEnabled(CustomComboPreset.MCH_Variant_Cure) &&
-                    IsEnabled(Variant.VariantCure) && PlayerHealthPercentageHp() <= Config.MCH_VariantCure)
+                    IsEnabled(Variant.VariantCure) &&
+                    PlayerHealthPercentageHp() <= Config.MCH_VariantCure)
                         return Variant.VariantCure;
 
                     if (IsEnabled(CustomComboPreset.MCH_Variant_Rampart) &&
@@ -460,7 +461,7 @@ namespace XIVSlothCombo.Combos.PvE
                         if (openerSelection is 0)
                         {
                             // Check to start opener
-                            if (openerStarted && WasLastAction(HeatedSplitShot) && gauge.Heat is 5 && InCombat() && GetRemainingCharges(GaussRound) is 3)
+                            if (openerStarted && WasLastAction(HeatedSplitShot))
                             {
                                 inOpener = true;
                                 openerStarted = false;
@@ -493,177 +494,178 @@ namespace XIVSlothCombo.Combos.PvE
                             }
 
                             // Reset if opener is interrupted, requires step 0 and 1 to be explicit since the inCombat check can be slow
-                            if ((step is 1 && WasLastAction(GaussRound)) ||
+                            if ((step is 1 && WasLastAction(GaussRound) && gauge.Heat > 5) ||
                                 (inOpener && step >= 2 && IsOffCooldown(actionID) && !InCombat()))
+                                inOpener = false;
 
-                                if (inOpener)
+                            if (inOpener)
+                            {
+                                //we do it in steps to be able to control it
+                                if (step == 0)
                                 {
-                                    //we do it in steps to be able to control it
-                                    if (step == 0)
-                                    {
-                                        if (WasLastAction(GaussRound)) step++;
-                                        else return GaussRound;
-                                    }
-
-                                    if (step == 1)
-                                    {
-                                        if (WasLastAction(Ricochet)) step++;
-                                        else return Ricochet;
-                                    }
-
-                                    if (step == 2)
-                                    {
-                                        if (IsOnCooldown(Drill)) step++;
-                                        else return Drill;
-                                    }
-
-                                    if (step == 3)
-                                    {
-                                        if (IsOnCooldown(BarrelStabilizer)) step++;
-                                        else return BarrelStabilizer;
-                                    }
-
-                                    if (step == 4)
-                                    {
-                                        if (WasLastWeaponskill(HeatedSlugshot)) step++;
-                                        else return HeatedSlugshot;
-                                    }
-
-                                    if (step == 5)
-                                    {
-                                        if (WasLastAction(Ricochet)) step++;
-                                        else return Ricochet;
-                                    }
-
-                                    if (step == 6)
-                                    {
-                                        if (WasLastWeaponskill(HeatedCleanShot)) step++;
-                                        else return HeatedCleanShot;
-                                    }
-
-                                    if (step == 7)
-                                    {
-                                        if (GetRemainingCharges(Reassemble) is 1) step++;
-                                        else return Reassemble;
-                                    }
-
-                                    if (step == 8)
-                                    {
-                                        if (WasLastAction(GaussRound)) step++;
-                                        else return GaussRound;
-                                    }
-
-                                    if (step == 9)
-                                    {
-                                        if (IsOnCooldown(AirAnchor)) step++;
-                                        else return AirAnchor;
-                                    }
-
-                                    if (step == 10)
-                                    {
-                                        if (GetRemainingCharges(Reassemble) is 0) step++;
-                                        else return Reassemble;
-                                    }
-
-                                    if (step == 11)
-                                    {
-                                        if (IsOnCooldown(Wildfire)) step++;
-                                        else return Wildfire;
-                                    }
-
-                                    if (step == 12)
-                                    {
-                                        if (IsOnCooldown(ChainSaw)) step++;
-                                        else return ChainSaw;
-                                    }
-
-                                    if (step == 13)
-                                    {
-                                        if (IsOnCooldown(AutomatonQueen)) step++;
-                                        else return AutomatonQueen;
-                                    }
-
-                                    if (step == 14)
-                                    {
-                                        if (IsOnCooldown(Hypercharge)) step++;
-                                        else return Hypercharge;
-                                    }
-
-                                    if (step == 15)
-                                    {
-                                        if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 4) step++;
-                                        else return HeatBlast;
-                                    }
-
-                                    if (step == 16)
-                                    {
-                                        if (WasLastAction(Ricochet)) step++;
-                                        else return Ricochet;
-                                    }
-
-                                    if (step == 17)
-                                    {
-                                        if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 3) step++;
-                                        else return HeatBlast;
-                                    }
-
-                                    if (step == 18)
-                                    {
-                                        if (WasLastAction(GaussRound)) step++;
-                                        else return GaussRound;
-                                    }
-
-                                    if (step == 19)
-                                    {
-                                        if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 2) step++;
-                                        else return HeatBlast;
-                                    }
-
-                                    if (step == 20)
-                                    {
-                                        if (WasLastAction(Ricochet)) step++;
-                                        else return Ricochet;
-                                    }
-
-                                    if (step == 21)
-                                    {
-                                        if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 1) step++;
-                                        else return HeatBlast;
-                                    }
-
-                                    if (step == 22)
-                                    {
-                                        if (WasLastAction(GaussRound)) step++;
-                                        else return GaussRound;
-                                    }
-
-                                    if (step == 23)
-                                    {
-                                        if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 0) step++;
-                                        else return HeatBlast;
-                                    }
-
-                                    if (step == 24)
-                                    {
-                                        if (WasLastAction(Ricochet)) step++;
-                                        else return Ricochet;
-                                    }
-
-                                    if (step == 25)
-                                    {
-                                        if (WasLastAction(Drill)) step++;
-                                        else return Drill;
-                                    }
-
-                                    inOpener = false;
+                                    if (WasLastAction(GaussRound)) step++;
+                                    else return GaussRound;
                                 }
+
+                                if (step == 1)
+                                {
+                                    if (WasLastAction(Ricochet)) step++;
+                                    else return Ricochet;
+                                }
+
+                                if (step == 2)
+                                {
+                                    if (IsOnCooldown(Drill)) step++;
+                                    else return Drill;
+                                }
+
+                                if (step == 3)
+                                {
+                                    if (IsOnCooldown(BarrelStabilizer)) step++;
+                                    else return BarrelStabilizer;
+                                }
+
+                                if (step == 4)
+                                {
+                                    if (WasLastWeaponskill(HeatedSlugshot)) step++;
+                                    else return HeatedSlugshot;
+                                }
+
+                                if (step == 5)
+                                {
+                                    if (WasLastAction(Ricochet)) step++;
+                                    else return Ricochet;
+                                }
+
+                                if (step == 6)
+                                {
+                                    if (WasLastWeaponskill(HeatedCleanShot)) step++;
+                                    else return HeatedCleanShot;
+                                }
+
+                                if (step == 7)
+                                {
+                                    if (GetRemainingCharges(Reassemble) is 1) step++;
+                                    else return Reassemble;
+                                }
+
+                                if (step == 8)
+                                {
+                                    if (WasLastAction(GaussRound)) step++;
+                                    else return GaussRound;
+                                }
+
+                                if (step == 9)
+                                {
+                                    if (IsOnCooldown(AirAnchor)) step++;
+                                    else return AirAnchor;
+                                }
+
+                                if (step == 10)
+                                {
+                                    if (GetRemainingCharges(Reassemble) is 0) step++;
+                                    else return Reassemble;
+                                }
+
+                                if (step == 11)
+                                {
+                                    if (IsOnCooldown(Wildfire)) step++;
+                                    else return Wildfire;
+                                }
+
+                                if (step == 12)
+                                {
+                                    if (IsOnCooldown(ChainSaw)) step++;
+                                    else return ChainSaw;
+                                }
+
+                                if (step == 13)
+                                {
+                                    if (IsOnCooldown(AutomatonQueen)) step++;
+                                    else return AutomatonQueen;
+                                }
+
+                                if (step == 14)
+                                {
+                                    if (IsOnCooldown(Hypercharge)) step++;
+                                    else return Hypercharge;
+                                }
+
+                                if (step == 15)
+                                {
+                                    if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 4) step++;
+                                    else return HeatBlast;
+                                }
+
+                                if (step == 16)
+                                {
+                                    if (WasLastAction(Ricochet)) step++;
+                                    else return Ricochet;
+                                }
+
+                                if (step == 17)
+                                {
+                                    if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 3) step++;
+                                    else return HeatBlast;
+                                }
+
+                                if (step == 18)
+                                {
+                                    if (WasLastAction(GaussRound)) step++;
+                                    else return GaussRound;
+                                }
+
+                                if (step == 19)
+                                {
+                                    if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 2) step++;
+                                    else return HeatBlast;
+                                }
+
+                                if (step == 20)
+                                {
+                                    if (WasLastAction(Ricochet)) step++;
+                                    else return Ricochet;
+                                }
+
+                                if (step == 21)
+                                {
+                                    if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 1) step++;
+                                    else return HeatBlast;
+                                }
+
+                                if (step == 22)
+                                {
+                                    if (WasLastAction(GaussRound)) step++;
+                                    else return GaussRound;
+                                }
+
+                                if (step == 23)
+                                {
+                                    if (WasLastAction(HeatBlast) && GetBuffStacks(Buffs.Overheated) is 0) step++;
+                                    else return HeatBlast;
+                                }
+
+                                if (step == 24)
+                                {
+                                    if (WasLastAction(Ricochet)) step++;
+                                    else return Ricochet;
+                                }
+
+                                if (step == 25)
+                                {
+                                    if (WasLastAction(Drill)) step++;
+                                    else return Drill;
+                                }
+
+                                inOpener = false;
+                            }
                         }
 
                         //123Tools Opener
                         if (openerSelection is 1)
                         {
                             // Check to start opener
-                            if (openerStarted && WasLastAction(HeatedSplitShot) && gauge.Heat is 5 && InCombat() && GetRemainingCharges(GaussRound) is 3)
+                            if (openerStarted && WasLastAction(HeatedSplitShot) && gauge.Heat is 5)
                             {
                                 inOpener = true;
                                 openerStarted = false;
@@ -697,8 +699,8 @@ namespace XIVSlothCombo.Combos.PvE
                             }
 
                             // Reset if opener is interrupted, requires step 0 and 1 to be explicit since the InCombat() check can be slow
-                            if ((step is 1 && WasLastAction(GaussRound) && gauge.Heat > 5) ||
-                                (inOpener && step >= 2 && IsOffCooldown(actionID) && !InCombat()))
+                            if ((step is 0 && WasLastAction(GaussRound) && gauge.Heat > 5) ||
+                                (inOpener && step >= 1 && IsOffCooldown(actionID) && !InCombat()))
                                 inOpener = false;
 
                             if (inOpener)
@@ -851,9 +853,10 @@ namespace XIVSlothCombo.Combos.PvE
                                 if (step == 24)
                                 {
                                     if (WasLastWeaponskill(HeatedSplitShot)) step++;
-                                    else return HeatedSplitShot; inOpener = false;
+                                    else return HeatedSplitShot;
                                 }
 
+                                inOpener = false;
                             }
                         }
 
