2cd671cb1393de889b72fd323c12a1d93c5ebd76
Kage <wijnsmattia@gmail.com>
2023-10-15 13:51:13 +0200

    fix sge bad import

--- XIVSlothCombo/Combos/CustomComboPreset.cs
+++ XIVSlothCombo/Combos/CustomComboPreset.cs
@@ -2783,11 +2783,7 @@ namespace XIVSlothCombo.Combos
 
         [ParentCombo(SGE_ST_Heal)]
         [CustomComboInfo("Eukrasian Diagnosis Option", "Diagnosis becomes Eukrasian Diagnosis if the shield is not applied to the target.", SGE.JobID, 313, "", "")]
-        SGE_ST_Heal_Diagnosis = 14320,
-
-        [ParentCombo(SGE_ST_Heal_Diagnosis)]
-        [CustomComboInfo("Ignore Shield Check", "Warning, will force the use of Eukrasia Diagnosis, and normal Diagnosis will be unavailable.", SGE.JobID)]
-        SGE_ST_Heal_Diagnosis_IgnoreShield = 14321,
+        SGE_ST_Heal_EDiagnosis = 14320,
 
         [ParentCombo(SGE_ST_Heal)]
         [CustomComboInfo("Soteria Option", "Applies Soteria.", SGE.JobID, 306, "", "")]
