8f8ef46ab6fccfa96ad7de1d896fe1f8580c4d8f
Taurenkey <github@taurenkey.co.uk>
2024-06-26 21:25:26 +0100

    Update PCT & VPR, remove PVP IDs

--- XIVSlothCombo/Combos/PvE/VPR.cs
+++ XIVSlothCombo/Combos/PvE/VPR.cs
@@ -11,32 +11,27 @@ namespace XIVSlothCombo.Combos.PvE
         public const byte JobID = 41;
 
         public const uint
+            SteelFangs = 34606,
             DreadFangs = 34607,
+            SteelMaw = 34614,
             DreadMaw = 34615,
             Dreadwinder = 34620,
             HuntersCoil = 34621,
-            HuntersDen = 34624,
-            HuntersSnap = 39166,
-            PitofDread = 34623,
-            RattlingCoil = 39189,
-            Reawaken = 34626,
-            SerpentsIre = 34647,
-            SerpentsTail1 = 35920,
-            SerpentsTail2 = 39183,
-            Slither1 = 34646,
-            Slither2 = 39184,
-            SnakeScales = 39185,
-            SteelFangs = 34606,
-            SteelMaw = 34614,
             SwiftskinsCoil = 34622,
+            PitofDread = 34623,
+            HuntersDen = 34624,
             SwiftskinsDen = 34625,
-            Twinblood = 35922,
+            Reawaken = 34626,
+            WrithingSnap = 34632,
+            UncoiledFury = 34633,
+            Slither = 34646,
+            SerpentsIre = 34647,
+            SerpentsTail = 35920,
             Twinfang = 35921,
-            UncoiledFury1 = 34633,
-            UncoiledFury2 = 39168,
-            Worldswallower = 39190, 
-            WrithingSnap = 34632;
- 
+            Twinblood = 35922;
+
+
+
         public static class Buffs
         {
 
