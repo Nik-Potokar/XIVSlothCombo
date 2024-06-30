e1e226cc0ab56d332751369dc6e73aa801cd26c9
Taurenkey <github@taurenkey.co.uk>
2024-06-30 19:11:02 +0100

    Fix cooldown data

--- XIVSlothCombo/Data/CustomComboCache.cs
+++ XIVSlothCombo/Data/CustomComboCache.cs
@@ -81,46 +81,18 @@ namespace XIVSlothCombo.Data
             if (actionManager == null)
                 return cooldownCache[actionID] = default;
 
-            byte cooldownGroup = GetCooldownGroup(actionID);
-
-            RecastDetail* cooldownPtr = actionManager->GetRecastGroupDetail(cooldownGroup - 1);
-            if (cooldownPtr is null)
+            CooldownData data = new()
             {
-                CooldownData data = new()
-                {
-                    CooldownTotal = -1
-                };
+                ActionID = actionID,
+            };
 
-                return cooldownCache[actionID] = data;
-            }
-
-            cooldownPtr->ActionId = actionID;
-
-            return cooldownCache[actionID] = *(CooldownData*)cooldownPtr;
+            return cooldownCache[actionID] = data;  
         }
 
         /// <summary> Get the maximum number of charges for an action. </summary>
         /// <param name="actionID"> Action ID to check. </param>
-        /// <returns> Max number of charges at current and max level. </returns>
-        internal unsafe (ushort Current, ushort Max) GetMaxCharges(uint actionID)
-        {
-            IPlayerCharacter? player = Service.ClientState.LocalPlayer;
-            if (player == null)
-                return (0, 0);
-
-            uint job = player.ClassJob.Id;
-            byte level = player.Level;
-            if (job == 0 || level == 0)
-                return (0, 0);
-
-            var key = (actionID, job, level);
-            if (chargesCache.TryGetValue(key, out var found))
-                return found;
-
-            ushort cur = ActionManager.GetMaxCharges(actionID, 0);
-            ushort max = ActionManager.GetMaxCharges(actionID, 90);
-            return chargesCache[key] = (cur, max);
-        }
+        /// <returns> Max number of charges at current level. </returns>
+        internal unsafe ushort GetMaxCharges(uint actionID) => ActionManager.GetMaxCharges(actionID, 0);
 
         /// <summary> Get the resource cost of an action. </summary>
         /// <param name="actionID"> Action ID to check. </param>
