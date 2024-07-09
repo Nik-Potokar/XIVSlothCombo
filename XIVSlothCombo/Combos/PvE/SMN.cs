86291fa171808f8c37272ba733fdbb9fa48d3d8a
Genesis <12591123+Genesis-Nova@users.noreply.github.com>
2024-07-09 10:53:53 -0500

    Pre-level 100 logic fix for smn burst delay, errant ! made the check always true.

--- XIVSlothCombo/Combos/PvE/SMN.cs
+++ XIVSlothCombo/Combos/PvE/SMN.cs
@@ -513,7 +513,7 @@ namespace XIVSlothCombo.Combos.PvE
                         // Demi Nuke
                         if (OriginalHook(Ruin) is AstralImpulse or UmbralImpulse or FountainOfFire)
                         {
-                            if (IsEnabled(CustomComboPreset.SMN_Advanced_Combo_DemiSummons_Attacks) && IsBahamutReady && (!LevelChecked(SummonSolarBahamut) || DemiAttackCount >= burstDelay))
+                            if (IsEnabled(CustomComboPreset.SMN_Advanced_Combo_DemiSummons_Attacks) && IsBahamutReady && (LevelChecked(SummonSolarBahamut) || DemiAttackCount >= burstDelay))
                             {
                                 if (IsOffCooldown(OriginalHook(EnkindleBahamut)) && LevelChecked(SummonBahamut))
                                     return OriginalHook(EnkindleBahamut);
