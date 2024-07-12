1dc3b0e1e3f819d6b97672cf59eac5087e286565
Thomas Henley <thomas.e.henley@gmail.com>
2024-07-12 11:43:36 -0400

    minor wording changes

--- XIVSlothCombo/Combos/CustomComboPreset.cs
+++ XIVSlothCombo/Combos/CustomComboPreset.cs
@@ -3783,7 +3783,7 @@ namespace XIVSlothCombo.Combos
         VPR_DreadwinderCoils = 30200,
 
         [ReplaceSkill(VPR.PitofDread)]
-        [CustomComboInfo("Pit Of Dread - Dens", "Replaces Pits Of Dread with the Dens.\n Also adds Twinfang and Twinblood to the button.", VPR.JobID)]
+        [CustomComboInfo("Pit Of Dread - Dens", "Replaces Pit Of Dread with Hunter's/Swiftskin's Dens.", VPR.JobID)]
         VPR_PitOfDreadDens = 30201,
 
         [ReplaceSkill(VPR.UncoiledFury)]
@@ -3803,11 +3803,11 @@ namespace XIVSlothCombo.Combos
         VPR_TwinTails = 30205,
 
         [ParentCombo(VPR_DreadwinderCoils)]
-        [CustomComboInfo("Include Twin Combo Actions", "Also add Twinfang and Twinblood to the button.", VPR.JobID)]
+        [CustomComboInfo("Include Twin Combo Actions", "Adds Twinfang and Twinblood to the button.", VPR.JobID)]
         VPR_DreadwinderCoils_oGCDs = 30206,
 
         [ParentCombo(VPR_PitOfDreadDens)]
-        [CustomComboInfo("Include Twin Combo Actions", "Also add Twinfang and Twinblood to the button.", VPR.JobID)]
+        [CustomComboInfo("Include Twin Combo Actions", "Adds Twinfang and Twinblood to the button.", VPR.JobID)]
         VPR_PitOfDreadDens_oGCDs = 30207,
 
         #endregion
