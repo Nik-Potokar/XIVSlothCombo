d8c833e9eb77516c338bc8b87ceed7a5c589b3ae
Ethan Henderson <ethan@zbee.codes>
2024-07-30 01:20:11 -0600

    Correct minimum, implement a maximum for Cure III's slider

--- XIVSlothCombo/Window/Functions/UserConfig.cs
+++ XIVSlothCombo/Window/Functions/UserConfig.cs
@@ -2527,7 +2527,7 @@ namespace XIVSlothCombo.Window.Functions
                 UserConfig.DrawSliderInt(0, 1, WHM.Config.WHM_AoEHeals_ThinAir, "How many charges to keep ready? (0 = Use all)");
 
             if (preset == CustomComboPreset.WHM_AoEHeals_Cure3)
-                UserConfig.DrawSliderInt(0, 10000, WHM.Config.WHM_AoEHeals_Cure3MP, "Use when MP is above", sliderIncrement: 100);
+                UserConfig.DrawSliderInt(1500, 8500, WHM.Config.WHM_AoEHeals_Cure3MP, "Use when MP is above", sliderIncrement: 500);
 
             if (preset == CustomComboPreset.WHM_STHeals)
                 UserConfig.DrawAdditionalBoolChoice(WHM.Config.WHM_STHeals_UIMouseOver, "Party UI Mouseover Checking", "Check party member's HP & Debuffs by using mouseover on the party list.\nTo be used in conjunction with Redirect/Reaction/etc.");
