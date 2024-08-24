026ac96bb187d68f2ddee09cd77610b57339e3da
Tartarga <109563717+Tartarga@users.noreply.github.com>
2024-08-23 19:44:14 -0500

    [SCH] Removed duplicate AoE Lucid Slider

--- XIVSlothCombo/Window/Functions/UserConfig.cs
+++ XIVSlothCombo/Window/Functions/UserConfig.cs
@@ -2208,9 +2208,6 @@ namespace XIVSlothCombo.Window.Functions
                 }
             }
 
-            if (preset is CustomComboPreset.SCH_AoE_Lucid)
-                UserConfig.DrawSliderInt(4000, 9500, SCH.Config.SCH_AoE_LucidOption, "MP Threshold", 150, SliderIncrements.Hundreds);
-
             if (preset is CustomComboPreset.SCH_ST_Heal)
             {
                 UserConfig.DrawAdditionalBoolChoice(SCH.Config.SCH_ST_Heal_Adv, "Advanced Options", "", isConditionalChoice: true);
@@ -2237,7 +2234,7 @@ namespace XIVSlothCombo.Window.Functions
             if (preset is CustomComboPreset.SCH_ST_Heal_Esuna)
                 UserConfig.DrawSliderInt(0, 100, SCH.Config.SCH_ST_Heal_EsunaOption, "Stop using when below HP %. Set to Zero to disable this check");
 
-            if (preset is CustomComboPreset.SCH_AoE_Heal)
+            if (preset is CustomComboPreset.SCH_AoE_Lucid)
                 UserConfig.DrawSliderInt(4000, 9500, SCH.Config.SCH_AoE_Heal_LucidOption, "MP Threshold", 150, SliderIncrements.Hundreds);
 
             if (preset is CustomComboPreset.SCH_DeploymentTactics)
