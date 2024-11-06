5c9a8996522a345761176b47c9d7f7fc230e5ad3
Kage <wijnsmattia@gmail.com>
2024-11-06 09:34:07 +0100

    fix gauge

--- XIVSlothCombo/Combos/JobHelpers/SGE.cs
+++ XIVSlothCombo/Combos/JobHelpers/SGE.cs
@@ -11,21 +11,15 @@ namespace XIVSlothCombo.Combos.JobHelpers;
 
 internal class SGE
 {
-    public static SGEGauge Gauge = GetJobGauge<SGEGauge>();
+    public static SGEGauge Gauge => GetJobGauge<SGEGauge>();
 
     public static int Dosis3Count => ActionWatching.CombatActions.Count(x => x == Dosis3);
 
     public static int Toxikon2Count => ActionWatching.CombatActions.Count(x => x == Toxikon2);
 
-    public static bool HasAddersgall(SGEGauge gauge)
-    {
-        return gauge.Addersgall > 0;
-    }
+    public static bool HasAddersgall(SGEGauge gauge) => gauge.Addersgall > 0;
 
-    public static bool HasAddersting(SGEGauge gauge)
-    {
-        return gauge.Addersting > 0;
-    }
+    public static bool HasAddersting(SGEGauge gauge) => gauge.Addersting > 0;
 
     internal class SGEOpenerLogic
     {
